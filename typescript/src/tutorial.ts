5b0000b5fcc1663b922aa211cd0790cd6f28535f
newdevmike22 <paola@dominicansoul.com>
2025-11-26 19:39:59 -0400

    Working with variable types
    Working with variable types: string, number, and boolean in TypeScript.

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -1,1 +1,38 @@
-console.log("typescript tutorial");
+let awesomeName = "shakeAndBake";
+awesomeName = "Marvelous Mighty Mike";
+awesomeName = awesomeName.toLocaleUpperCase();
+console.log(awesomeName);
+
+let amount = 57;
+amount = 25 + 10;
+console.log(amount);
+
+let isStudent = true;
+isStudent = false;
+console.log(isStudent);
+
+/* Challenge
+- Create a variable of type string and try to invoke a string method on it.
+- Create a variable of type number and try to perform a mathmatical operation on it.
+- Create a variable of type boolean and try to perform a logical operation on it.
+- Assign the wrong type to each variable to see what happens.
+*/
+
+let greatGuitarPlayer: string = "Jimi Hendix";
+greatGuitarPlayer = greatGuitarPlayer.toUpperCase();
+console.log(greatGuitarPlayer);
+
+let weight: number = 250;
+weight = weight + 175;
+console.log(`Tim and his brother weigh a combined ${weight} pounds.`);
+
+let isOverWeight: boolean = weight >= 300;
+console.log(isOverWeight);
+
+/*
+greatGuitarPlayer = 50;
+weight = "seventy";
+isOverWeight = "no";
+*/
+
+console.log(greatGuitarPlayer, weight, isOverWeight);
