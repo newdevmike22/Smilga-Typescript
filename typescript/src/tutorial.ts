b855a35c94afbdf16c9987da8778c2846bb7d39c
newdevmike22 <paola@dominicansoul.com>
2025-11-27 09:37:15 -0400

    Working with union, any, and annotation
    Working with union, any, and annotation types in TypeScript.

--- typescript/src/tutorial.ts
+++ typescript/src/tutorial.ts
@@ -1,3 +1,4 @@
+/*
 let awesomeName = "shakeAndBake";
 awesomeName = "Marvelous Mighty Mike";
 awesomeName = awesomeName.toLocaleUpperCase();
@@ -18,6 +19,7 @@ console.log(isStudent);
 - Assign the wrong type to each variable to see what happens.
 */
 
+/*
 let greatGuitarPlayer: string = "Jimi Hendix";
 greatGuitarPlayer = greatGuitarPlayer.toUpperCase();
 console.log(greatGuitarPlayer);
@@ -35,4 +37,54 @@ weight = "seventy";
 isOverWeight = "no";
 */
 
-console.log(greatGuitarPlayer, weight, isOverWeight);
+//console.log(greatGuitarPlayer, weight, isOverWeight);
+
+// Union type
+let tax: number | string = 10;
+tax = 100;
+tax = "$10";
+console.log(tax);
+
+let requestStatus: "pending" | "success" | "error" = "pending";
+requestStatus = "success";
+requestStatus = "error";
+console.log(requestStatus);
+
+// Any type
+let notSure: any = 4;
+notSure = "I might use a number instead.";
+notSure = false;
+console.log(notSure);
+
+// Type Annotation
+const books = ["1984", "Brave New World", "The Richest Man In Babylon"];
+
+let foundBook: string | undefined;
+
+for (let book of books) {
+  if (book === "1984") {
+    foundBook = book;
+    foundBook = foundBook.toLocaleUpperCase();
+    break;
+  }
+}
+
+console.log(foundBook?.length);
+
+/*
+## Challenge
+- Create a variable orderStatus of type 'processing' | 'shipped' | 'delivered' and
+assign it the value 'processing'. Then, try to assign it the values 'shipped' and 'delivered'.
+
+- Create a variable discount of type number | string and assign it the value 20.
+Then, try to assign it the value '20%'.
+*/
+
+let orderStatus: "processing" | "shipped" | "delivered" = "processing";
+orderStatus = "shipped";
+orderStatus = "delivered";
+console.log(orderStatus);
+
+let discount: number | string = 20;
+discount = "20%";
+console.log(discount);
